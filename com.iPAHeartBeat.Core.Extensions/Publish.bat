47a3e53dcf3fc77205ee0a8d8102aa26d74b035b
Ankur Ranpariya <ankur30884@gmail.com>
2022-09-28 01:25:09 +0530

    Supportive files Added like - .editorconfig - .styleCop - Directory Build Props for Analyzer - Licence File - XML Doc file created with build. - Batch file to build and publish Dll to unity package folder.

---
+++ com.iPAHeartBeat.Core.Extensions/Publish.bat
@@ -0,0 +1,2 @@
+dotnet publish com.iPAHeartBeat.Core.Extensions.csproj -c Release --no-dependencies --framework net48 --output ../Packages/com.iPAHeartBeat.Core.Extensions/Scripts/Runtime/.
+del ..\Packages\com.iPAHeartBeat.Core.Extensions\Scripts\Runtime\Newtonsoft.Json.dll
