90cd481040835e18f7aac2cf526afc068e9d018c
Ankur Ranpariya <ankur30884@gmail.com>
2022-09-28 19:36:49 +0530

    Main project file updated for version of the output. PublishForUnityNpm is updated for output folder path and remove NewtonSoft.Json dll from Npm

--- iPAHeartBeat.Core/PublishForUnityNpm.bat
+++ iPAHeartBeat.Core/PublishForUnityNpm.bat
@@ -1,2 +1,2 @@
-dotnet publish com.iPAHeartBeat.Core.Extensions.csproj -c Release --no-dependencies --framework net48 --output ../Packages/com.iPAHeartBeat.Core.Extensions/Scripts/Runtime/.
-del ..\Packages\com.iPAHeartBeat.Core.Extensions\Scripts\Runtime\Newtonsoft.Json.dll
+dotnet publish com.iPAHeartBeat.Core.Extensions.csproj -c Release --no-dependencies --framework net48 --output ../Packages/com.iPAHeartBeat.Core.Extensions/Runtime/.
+del ..\Packages\com.iPAHeartBeat.Core.Extensions\Runtime\Newtonsoft.Json.dll
