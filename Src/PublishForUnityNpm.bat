a6597ca6068b6ef645408ef80062c3c5cc9b3aa3
Ankur Ranpariya <ankur30884@gmail.com>
2022-10-13 22:25:24 +0530

    project file updated for git Url publish bat file updated with export file to remove extra dll.

--- Src/PublishForUnityNpm.bat
+++ Src/PublishForUnityNpm.bat
@@ -1,2 +1,2 @@
 dotnet publish com.iPAHeartBeat.Core.Extensions.csproj -c Release --no-dependencies --framework net48 --output ../Unity/Packages/com.iPAHeartBeat.Core.Extensions/Runtime/.
-del ..\Packages\com.iPAHeartBeat.Core.Extensions\Runtime\Newtonsoft.Json.*
+del ..\Unity\Packages\com.iPAHeartBeat.Core.Extensions\Runtime\Newtonsoft.Json.*
