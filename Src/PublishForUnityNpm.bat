749e3b160041937d88e9a25febcb04a871292b5e
Ankur Ranpariya <ankur30884@gmail.com>
2022-12-01 22:04:02 +0530

    Removed Old Publish NPM batch file

--- Src/PublishForUnityNpm.bat
+++
@@ -1,2 +0,0 @@
-dotnet publish com.iPAHeartBeat.Core.Extensions.csproj -c Release --no-dependencies --framework net48 --output ../Unity/Packages/com.iPAHeartBeat.Core.Extensions/Runtime/.
-del ..\Unity\Packages\com.iPAHeartBeat.Core.Extensions\Runtime\Newtonsoft.Json.*
