59e647abc6aff58a594a1f8aebf18f9832b8674e
Ankur Ranpariya <ranpariya.ankur@nukeboxstudios.com>
2023-10-06 19:35:20 +0530

    BatFile for windows updated
    (cherry picked from commit 2bd3196b416ba453317fc0f52022447df7d77c2f)

--- CreatePacakges.bat
+++ CreatePacakges.bat
@@ -1,5 +1,7 @@
-@REM Build project for Realase
+@REM Build project for Relapse
 ECHO Building project for Release configuration.
+dotnet clean Src/. -c Debug
+dotnet clean Src/. -c Release
 dotnet build Src/. -c Release
 
 @REM Creating NuGet packages
@@ -9,23 +11,23 @@ dotnet pack Src/. --include-symbols --force -c Release --output ./Packages/NuGet
 @REM Creating Unity NPM Project
 ECHO Step 1: Publishing Code for Unity Package
 set location=com.iPAHeartBeat.Core.Extensions
+del ./Unity/Packages/%location%/Runtime/*.*
 dotnet publish Src/%location%.csproj -c Release --no-dependencies --framework net471 --output ./Unity/Packages/%location%/Runtime/.
 
-@REM Removing Extra DLL for Which Code Will avaialble via Unity package registries.
+@REM Removing Extra DLL for Which Code Will available via Unity package registries.
 ECHO Step 2: Removing Extra DLLs
-cd Unity/Packages/%location%/Runtime
+cd ./Unity/Packages/%location%/Runtime
 del Newtonsoft.Json.dll
-cd..
-cd..
-cd..
-cd..
+cd ..
+cd ..
+cd ..
+cd ..
 
 @REM Create Node package for Unity
-ECHO Step 3: Creating Node Package for Unity Pacakge Manger.
+ECHO Step 3: Creating Node Package for Unity Package Manager.
 cd Unity/Packages/%location%
 npm pack --pack-destination="../../../Packages/UnityNPM/"
 cd ..
 cd ..
 cd ..
-cls
-ECHO Pakcages are Created.
+ECHO Packages are Created.
